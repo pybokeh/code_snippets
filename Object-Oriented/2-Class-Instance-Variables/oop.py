a6ca951bdfe1bfce88cca2b916cd5dd9ecdf5587
pybokeh <pybokeh@gmail.com>
2020-04-12 20:39:44 -0400

    Added line to create class variable

--- Object-Oriented/2-Class-Instance-Variables/oop.py
+++ Object-Oriented/2-Class-Instance-Variables/oop.py
@@ -1,6 +1,8 @@
 
 class Employee:
 
+    raise_amount = 1.04   # class variable
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -10,5 +12,11 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+       #self.pay = int(self.pay * Employee.raise_amount) Could also do this
+
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+
+# To get "name space" of class instances: print(emp_1.__dict__)
